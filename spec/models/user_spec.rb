5671cce53ad6d23fb3ad28071f7972d5db6f7844
DylanGuedes <djmgguedes@gmail.com>
2015-01-24 12:29:14 -0200

    new tests for spec_user'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+include SessionsHelper
+
 RSpec.describe User, :type => :model do
   before do
     @user = FactoryGirl.create(:user)
@@ -12,4 +14,26 @@ RSpec.describe User, :type => :model do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:password_digest ) }
+
+  describe "user" do
+    it "#digest should be kind of bcrypt_password" do
+      action = User.digest(@user.password)
+      expect(action).to be_kind_of(BCrypt::Password)
+    end
+    it "#new_token should return a new crypted pass" do
+      action = User.new_token
+      expect(action).to be_kind_of(String)
+    end
+    describe "authenticated?" do
+      it "should return nil with an unlogged user" do
+        action = @user.authenticated?(@user.remember_token)
+        expect(action).to eq(false)
+      end
+    end
+    it "#forget should make remember_digest nil" do
+      @user.remember_digest = "123456"
+      action = @user.forget
+      expect(@user.remember_digest).to eq(nil)
+    end
+  end
 end
